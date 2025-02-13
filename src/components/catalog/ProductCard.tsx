5006ab59b4fea2c5bfcb480b932e04ec726641b9
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-13 18:49:09 -0300

    feat: teste

--- src/components/catalog/ProductCard.tsx
+++ src/components/catalog/ProductCard.tsx
@@ -2,6 +2,8 @@ import { Instagram, ChevronLeft, ChevronRight, X, Maximize2 } from 'lucide-react
 import { Product } from '../../types/product';
 import { useState, useEffect } from 'react';
 import { adminApi } from '../../services/api';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
 
 interface ProductCardProps {
   product: Product;
@@ -46,44 +48,46 @@ export function ProductCard({ product, instagramUsername }: ProductCardProps) {
   return (
     <div className="bg-white rounded-lg shadow-sm overflow-hidden">
       <div className="relative w-full h-80 flex items-center justify-center">
-        {imageUrls.length > 0 ? (
-          <>
-            <button
-              onClick={handlePrevImage}
-              className="absolute left-2 text-purple-600 hover:text-purple-800 bg-white p-1 rounded-full shadow"
-            >
-              <ChevronLeft size={24} />
-            </button>
-
+      {imageUrls.length > 0 ? (
+    <>
+      <Swiper
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Navigation, Pagination]}
+        className="w-full h-full"
+      >
+        {imageUrls.map((url, index) => (
+          <SwiperSlide key={index}>
             <img
               onClick={openModal}
-              src={imageUrls[currentImageIndex]}
+              src={url}
               alt={product.name}
-              className="w-full h-full object-fill"  
+              className="w-full h-full object-cover"
             />
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
-            <button
-              onClick={handleNextImage}
-              className="absolute right-2 text-purple-600 hover:text-purple-800 bg-white p-1 rounded-full shadow"
-            >
-              <ChevronRight size={24} />
-            </button>
-
-            {/* Botão para abrir o modal */}
-            <button
-              onClick={openModal}
-              className="absolute bottom-2 right-2 text-purple-600 hover:text-purple-800 bg-white p-1 rounded-full shadow"
-            >
-              <Maximize2 size={20} /> {/* Ícone de fullscreen */}
-            </button>
-          </>
-        ) : (
-          <img
-            src="/default-image.jpg" 
-            alt={product.name}
-            className="w-full h-full object-cover" 
-          />
-        )}
+      {/* Botão para abrir o modal */}
+      <button
+        onClick={openModal}
+        className="absolute bottom-2 right-2 text-purple-600 hover:text-purple-800 bg-white p-1 rounded-full shadow z-10"
+      >
+        <Maximize2 size={20} /> {/* Ícone de fullscreen */}
+      </button>
+    </>
+  ) : (
+    <img
+      src="/default-image.jpg"
+      alt={product.name}
+      className="w-full h-full object-cover"
+    />
+  )}
 
         <div className="absolute top-2 right-2 bg-pink-100 px-3 py-1 rounded-full">
           <span className="text-sm font-medium text-pink-600">R$ {product.priceRange}</span>
