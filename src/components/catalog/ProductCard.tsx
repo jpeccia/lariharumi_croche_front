3d0f68792fdbfe0c13a891578ca7eb2abcde6550
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-18 23:38:34 -0300

    feat: add webp in product card

--- src/components/catalog/ProductCard.tsx
+++ src/components/catalog/ProductCard.tsx
@@ -54,11 +54,10 @@ export function ProductCard({ product, instagramUsername }: ProductCardProps) {
             <ChevronLeft size={24} />
           </button>
                 <img
-                  loading="lazy"
-                  onClick={openModal}
-                  src={imageUrls[currentImageIndex]}
-                  alt={product.name}
-                  className="w-full h-full object-fill"  
+                loading="lazy"
+                onClick={openModal}
+                srcSet={`${imageUrls[currentImageIndex]}.webp 1x, ${imageUrls[currentImageIndex]} 1x`}
+                src={imageUrls[currentImageIndex]}
                 />
           <button
             onClick={handleNextImage}
